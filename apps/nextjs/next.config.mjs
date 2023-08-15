8ebd49c9ea6f3bbb4342a74c787bdc4439207ab5
Mac Sweeny <macsweenydev@gmail.com>
2023-08-15 23:23:23 +1000

    feat: stores page

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -9,6 +9,16 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default config;
--- apps/nextjs/src/pages/stores/index.tsx
+++ apps/nextjs/src/pages/stores/index.tsx
@@ -1,91 +1,141 @@
-import { CreateOrganization, useOrganizationList, useOrganization } from "@clerk/nextjs";
-import DashboardLayout from "../../components/layouts/dashboard";
-import { Button } from "@tremor/react";
-import { useState } from "react";
-import { OrganizationMembershipResource, OrganizationResource, SetActive } from "@clerk/types";
+import { useState, type ReactNode } from "react";
+import Image from "next/image";
 import { useRouter } from "next/router";
+import {
+  CreateOrganization,
+  useOrganization,
+  useOrganizationList,
+} from "@clerk/nextjs";
+import {
+  type OrganizationMembershipResource,
+  type OrganizationResource,
+  type SetActive,
+} from "@clerk/types";
+import { Button, Card, Flex, Grid, Text, Title } from "@tremor/react";
 
+type StoreCardProps = {
+  organization: {
+    membership: OrganizationMembershipResource;
+    organization: OrganizationResource;
+  };
+  setActive: SetActive;
+  isActive: boolean;
+};
 
-function Store( { organization, setActive, isActive } : { organization: {
-  membership: OrganizationMembershipResource;
-  organization: OrganizationResource;
-}, setActive: SetActive, isActive: boolean}) {
-  const router = useRouter();
-
-  return (
-    <div className={`border p-2 rounded-md ${isActive ? "bg-lime-200 text-black" : ""}`}
-      onClick={() => {
-        setActive({organization: organization.organization}).then(() => {
-          router.push(`/stores/${organization.organization.slug}/overview`)
-        })
-      
-      }}
-    >
-      <div className="flex flex-row justify-center w-96">
-        {organization.organization.name}
-      </div>
-    </div>
-  )
-}
+type ModalContentProps = {
+  children: ReactNode;
+  onClose: () => void;
+};
 
 export default function Stores() {
-  const {organization: org, isLoaded: isLoadedOrg} = useOrganization();
-
-  const { 
+  const { organization: org, isLoaded: isLoadedOrg } = useOrganization();
+  const {
     isLoaded: isLoadedList,
     organizationList,
-    createOrganization,
-    setActive
-  } = useOrganizationList()
-
+    setActive,
+  } = useOrganizationList();
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <div>
-      <div className="container mx-auto">
-        <div className="flex flex-row justify-center pt-8">
-          <div className="flex flex-col space-y-2">
-            <div className="text-2xl text-center">
-              Pick a Store
-            </div>
-            {isLoadedList && isLoadedOrg && organizationList.map((organization) => (
-              <Store organization={organization} setActive={setActive} isActive={organization.organization.id === org?.id || false} />
-            ))}
-            {isLoadedList && isLoadedOrg && organizationList.length === 0 && (
-              <div className="text-lg">
-                You don't have any stores yet.
-              </div>
-            )}
-            {isLoadedList && isLoadedOrg && (
-              <Button
-                onClick={() => {
-                  setShowModal(true)
-                }}
-              >
-                Create Organization
-              </Button>
-            )}
-          </div>
+    <main className="m-8">
+      <Flex justifyContent="between">
+        <div>
+          <Title>Stores</Title>
+          <Text>
+            This is a list of all the stores you have access to. Click on a
+            store to view it.
+          </Text>
         </div>
-      </div>
-      {showModal ? (
+        {isLoadedOrg && isLoadedList && (
+          <Button onClick={() => setShowModal(true)}>Create Store</Button>
+        )}
+      </Flex>
+      {isLoadedList && isLoadedOrg && (
         <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-            onClick={() => setShowModal(false)}
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl"
-              onClick={e => e.stopPropagation()}
-            >
-              {/*content*/}
-              <CreateOrganization
-                afterCreateOrganizationUrl="/stores"
+          <Grid numItemsMd={3} numItemsSm={2} className="mt-6 gap-6">
+            {organizationList.map((organization) => (
+              <StoreCard
+                key={organization.organization.id}
+                organization={organization}
+                setActive={setActive}
+                isActive={organization.organization.id === org?.id}
               />
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            ))}
+          </Grid>
+          {showModal && (
+            <ModalContent onClose={() => setShowModal(false)}>
+              <CreateOrganization afterCreateOrganizationUrl="/stores" />
+            </ModalContent>
+          )}
         </>
-      ) : null}
-    </div>
+      )}
+    </main>
   );
 }
+
+const ModalContent: React.FC<ModalContentProps> = ({ children, onClose }) => {
+  return (
+    <>
+      <div
+        className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none"
+        onClick={onClose}
+      >
+        <div
+          className="relative mx-auto my-6 w-auto max-w-3xl"
+          onClick={(e) => e.stopPropagation()}
+        >
+          {children}
+        </div>
+      </div>
+      <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+    </>
+  );
+};
+
+const StoreCard: React.FC<StoreCardProps> = ({ organization, setActive }) => {
+  const router = useRouter();
+
+  const handleOrganizationClick = () => {
+    const executeAsyncOperation = async () => {
+      console.log("clicked");
+      try {
+        await setActive({ organization: organization.organization });
+        void router.push(`/stores/${organization.organization.slug}/overview`);
+      } catch (error) {
+        console.error("Error setting active organization:", error);
+      }
+    };
+
+    executeAsyncOperation().catch((error) => {
+      // Handle any unhandled promise rejections
+      console.error("Error in handleOrganizationClick:", error);
+    });
+  };
+
+  return (
+    <div
+      role="button"
+      tabIndex={0}
+      onClick={handleOrganizationClick}
+      onKeyDown={handleOrganizationClick}
+    >
+      <Card
+        key={organization.organization.id}
+        className="flex h-44 cursor-pointer items-center justify-center rounded-xl border border-neutral-600"
+      >
+        <div className="flex items-center gap-x-4">
+          <Image
+            src={organization.organization.imageUrl}
+            width={48}
+            height={48}
+            alt={organization.organization.name}
+            className="flex-none rounded-lg object-cover"
+          />
+          <Text className="text-xl font-medium leading-6 text-neutral-100">
+            {organization.organization.name}
+          </Text>
+        </div>
+      </Card>
+    </div>
+  );
+};
