d75a9707b79964c60f4bbf387c5909f5062340f2
Mac Sweeny <macsweenydev@gmail.com>
2023-08-16 20:52:07 +1000

    C.O.D

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,12 @@
 import { authRouter } from "./router/auth";
-import { postRouter } from "./router/post";
+import { costOfDeliveryRouter } from "./router/costOfDelivery";
 import { createTRPCRouter } from "./trpc";
+import { postRouter } from "./router/post";
 
 export const appRouter = createTRPCRouter({
   post: postRouter,
   auth: authRouter,
+  costOfDelivery: costOfDeliveryRouter,
 });
 
 // export type definition of API
---
+++ packages/api/src/router/costOfDelivery.ts
@@ -0,0 +1,97 @@
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+import { z } from "zod";
+
+export const costOfDeliveryInput = z.object({
+  name: z.string(),
+  value: z.number(),
+});
+
+export const costOfDeliveryRouter = createTRPCRouter({
+  getCostOfDelivery: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.costOfDeliveryLineItems.findMany({
+      where: {
+        organizationId: ctx.auth.organization?.id,
+      },
+    });
+  }),
+
+  createCostOfDelivery: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        value: z.number(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.costOfDeliveryLineItems.create({
+        data: {
+          name: input.name,
+          value: input.value,
+          organizationId: ctx.auth.organization?.id,
+        },
+      });
+    }),
+
+  getTotalCostOfDelivery: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.costOfDeliveryLineItems.aggregate({
+      _sum: {
+        value: true,
+      },
+      where: {
+        organizationId: ctx.auth.organization?.id,
+      },
+    });
+  }),
+
+  updateCostOfDelivery: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        name: z.string(),
+        value: z.number(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.costOfDeliveryLineItems.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          name: input.name,
+          value: input.value,
+        },
+      });
+    }),
+
+  deleteCostOfDelivery: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.costOfDeliveryLineItems.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+
+  selectCostOfDelivery: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        value: z.number(),
+      }),
+    )
+    .query(({ ctx, input }) => {
+      return ctx.prisma.costOfDeliveryLineItems.findMany({
+        where: {
+          organizationId: ctx.auth.organization?.id,
+          name: input.name,
+          value: input.value,
+        },
+      });
+    }),
+});
